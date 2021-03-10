ae66ef191046edb33b83c31e98fed3a33a1d0fd3
renaudesign <renaud.sourmail@gmail.com>
2021-03-10 16:43:20 +0100

    correction

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function green(){
-    document.getElementById("text").style.color = "#4dbc4f"
-}
-function blue(){
-    document.getElementById("text").style.color = "#489cea"
-}
-function red(){
-    document.getElementById("text").style.color = "#cc1e0e"
+function colorize(element){
+    document.getElementById("text").style.color = element.getAttribute("class").substring("6")
 }
